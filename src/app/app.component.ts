4fb49d3dd9f404519dc830705d989196e2b22c4d
cloudsony999 <cloudsony999@gmail.com>
2022-11-09 17:20:04 +0530

    mycommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
 import { Component } from '@angular/core';
 
+import { FormControl } from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo';
+  anudip = 'OUR APP';
+
+  ename="amitava";
+  age=50;
+
+  display()
+  {
+    alert('Welcome to Anudip!!!!')
+  }
+  uname=new FormControl("");
+  updateName()
+  {
+    this.uname.setValue("amitava");
+  }
+
+  leds:any=
+  {
+    title:"55 inch led",
+    brand:"Sony",
+    price:'45000',
+    description:'This is a Google TV'
+  }
+
+  audio:any=
+  [
+    {
+    title:"home theater",
+    brand:"Sony",
+    price:'40000',
+
+    },
+    {
+    title:"headphone",
+    brand:"Bose",
+    price:'30000',
+
+
+    },
+    {
+    title:"party speaker",
+    brand:"JBL",
+    price:'30000',
+
+
+    }
+
+  ]
+
+
+
+
+
+
+
+
+  
+
+
+
 }
